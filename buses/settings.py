40ee9d26a13cc65b01378f58997b0a7d0fcfad40
Kai <Kaiboucher2007@gmail.com>
2026-04-19 22:59:20 +0100

    Refactor ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- buses/settings.py
+++ buses/settings.py
@@ -14,12 +14,16 @@ from sentry_sdk.integrations.redis import RedisIntegration
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["SECRET_KEY"]
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split()
 
-CSRF_TRUSTED_ORIGINS = os.environ.get(
-    "CSRF_TRUSTED_ORIGINS",
-    "https://ukbuses.org",
-).split()
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.environ["ALLOWED_HOSTS"].split(",")
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in os.environ["CSRF_TRUSTED_ORIGINS"].split(",")
+]
 
 TEST = "test" in sys.argv or "pytest" in sys.argv[0]
 DEBUG = True
