4ee1985eccd9764f14335c59363f86a36de0504d
Brooklynbull321 <brook1988@outlook.com>
2026-04-20 03:35:28 +0100

    Update bigmap.js

--- frontend/js/bigmap.js
+++ frontend/js/bigmap.js
@@ -1,5 +1,7 @@
 import loadjs from "loadjs";
 
+window.loadJS = loadjs;
+
 // https://maplibre.org/maplibre-gl-js/docs/examples/check-for-support/
 function isWebglSupported() {
   if (window.WebGLRenderingContext) {
