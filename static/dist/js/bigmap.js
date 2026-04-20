abf6289ef3bbbef377b06202321a1ca52320019d
Brooklynbull321 <brook1988@outlook.com>
2026-04-20 03:17:34 +0100

    Revert "MAP Test"
    This reverts commit 4155c33f26b75c7fc844d507e36dc64ecffb6ba7.

--- static/dist/js/bigmap.js
+++ static/dist/js/bigmap.js
@@ -1,13 +1,15 @@
-// https://maplibre.org/maplibre-gl-js/docs/examples/check-for-support/
+import loadjs from "loadjs";
 
+// https://maplibre.org/maplibre-gl-js/docs/examples/check-for-support/
 function isWebglSupported() {
   if (window.WebGLRenderingContext) {
     const canvas = document.createElement("canvas");
-
     try {
-      const context =
-        canvas.getContext("webgl2") || canvas.getContext("webgl");
-
+      // Note that { failIfMajorPerformanceCaveat: true } can be passed as a second argument
+      // to canvas.getContext(), causing the check to fail if hardware rendering is not available. See
+      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+      // for more details.
+      const context = canvas.getContext("webgl2") || canvas.getContext("webgl");
       if (
         context &&
         typeof context.getParameter === "function" &&
@@ -16,25 +18,19 @@ function isWebglSupported() {
         return true;
       }
     } catch (e) {
-      // WebGL is supported but disabled
+      // WebGL is supported, but disabled
     }
     return false;
   }
   return false;
 }
 
-// Make sure LoadJS is available globally (from CDN in index.html)
-function loadScript() {
-  if (!window.loadjs) {
-    console.error("LoadJS is not loaded. Add the CDN script in index.html.");
-    return;
-  }
-
-  if (typeof window.fetch !== "undefined" && isWebglSupported()) {
-    window.loadjs(window.NEW_JS, { async: false });
-  } else {
-    window.loadjs(window.OLD_JS, { async: false });
-  }
+if (typeof window.fetch !== "undefined" && isWebglSupported()) {
+  loadjs(window.NEW_JS, {
+    async: false,
+  });
+} else {
+  loadjs(window.OLD_JS, {
+    async: false,
+  });
 }
-
-loadScript();
