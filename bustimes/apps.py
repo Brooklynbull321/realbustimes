d05a541326396c248c9243f62a12599bd7e12ccc
Brooklynbull321 <brook1988@outlook.com>
2026-04-29 01:19:55 +0100

    E

--- bustimes/apps.py
+++ bustimes/apps.py
@@ -4,5 +4,5 @@ from django.apps import AppConfig
 class BustimesConfig(AppConfig):
     name = "bustimes"
 
-    def ready(self):
-        import bustimes.signals
+def ready(self):
+    return  # disable signals temporarily
