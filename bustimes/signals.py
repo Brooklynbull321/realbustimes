6ec05395ca760e90359b9326b330df4051783788
Brooklynbull321 <brook1988@outlook.com>
2026-04-29 01:27:11 +0100

    Reapply "B"
    This reverts commit 7c0aa302a70881cff0c7b498cf0fc93a6e5011a5.

--- bustimes/signals.py
+++ bustimes/signals.py
@@ -5,35 +5,24 @@ from django.dispatch import receiver
 from .models import OperatorUser
 
 
-def trigger_local_lock_update():
-    """
-    Debounced trigger to prevent spam updates
-    when multiple OperatorUser changes happen quickly.
-    """
-
-    if cache.get("local_lock_update_lock"):
+def trigger_update():
+    if cache.get("local_lock_trigger_lock"):
         return
 
-    cache.set("local_lock_update_lock", True, timeout=5)
+    cache.set("local_lock_trigger_lock", True, timeout=5)
 
     try:
         from .local_lock import build_local_lock_dashboard
         build_local_lock_dashboard()
     finally:
-        cache.delete("local_lock_update_lock")
+        cache.delete("local_lock_trigger_lock")
 
 
-# -------------------------
-# CREATE / UPDATE
-# -------------------------
 @receiver(post_save, sender=OperatorUser)
-def operatoruser_saved(sender, instance, created, **kwargs):
-    trigger_local_lock_update()
+def saved(sender, instance, **kwargs):
+    trigger_update()
 
 
-# -------------------------
-# DELETE
-# -------------------------
 @receiver(post_delete, sender=OperatorUser)
-def operatoruser_deleted(sender, instance, **kwargs):
-    trigger_local_lock_update()
+def deleted(sender, instance, **kwargs):
+    trigger_update()
